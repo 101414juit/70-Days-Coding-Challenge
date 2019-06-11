145cb2d0020f50ac56b0e0880b68ecced5c789aa
Aman Rastogi <rastogiaman690@gmail.com>
2019-06-12 00:21:59 +0530

    Codechef June Long 2019

---
+++ Day 6/PROXYC.py
@@ -0,0 +1,30 @@
+'''
+https://www.codechef.com/JUNE19B/problems/PROXYC
+Custom Testcase:
+4
+8
+PAAAAAPA
+8
+APAAAAPA
+5
+PPPPP
+5
+AAAAA
+'''
+
+for _ in range(int(input())):
+    D = int(input())
+    s = input()
+    count_p=s.count('P')
+    counter=count_p
+    if count_p/D >=0.75:
+        print(0)
+    else:
+        for i in range (2,len(s)-2):
+            if s[i] == 'A' and ( s[i-1] == 'P' or s[i-2] == 'P'or s[i+1] == 'P' or s[i+2] == 'P' ):
+                counter += 1
+                if counter/D >= 0.75:
+                    print(counter-count_p)
+                    break
+        if counter/D < 0.75:
+            print('-1')
