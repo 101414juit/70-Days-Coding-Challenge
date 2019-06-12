0e9a96a5b65c4d7935ea92a6352b6df798a0663f
Aman Rastogi <rastogiaman690@gmail.com>
2019-06-12 17:43:23 +0530

    AC

--- Day 6/PROXYC.py
+++ Day 6/PROXYC.py
@@ -1,42 +1,25 @@
 '''
+Codechef Long Challenge 2019 June
+Problem Statement:
 https://www.codechef.com/JUNE19B/problems/PROXYC
-Custom Testcase:
-10
-2
-PP
-2
-PA
-1
-A
-1
-P
-4
-PAAP
-8
-PAAAAAPA
-8
-APAAAAPA
-5
-PPPPP
-5
-AAAAA
-9
-PAAPPAPPP
 '''
 
+import math
 for _ in range(int(input())):
     D = int(input())
     s = input()
     count_p=s.count('P')
-    counter=count_p
+    counter=0
     if count_p/D >=0.75:
-        print(0)
+        print(counter)
     else:
+        x=math.ceil( (0.75*D) - count_p )
+        # print('Value of X is {} '.format(x))
         for i in range (2,len(s)-2):
-            if s[i] == 'A' and ( s[i-1] == 'P' or s[i-2] == 'P'or s[i+1] == 'P' or s[i+2] == 'P' ):
-                counter += 1
-                if counter/D >= 0.75:
-                    print(counter-count_p)
+            if s[i] == 'A' and ( s[i-1] == 'P' or s[i-2] == 'P' ) and (s[i+1] == 'P' or s[i+2] == 'P' ):
+                counter = 1 + counter
+                if counter == x:
+                    print(counter)
                     break
-        if counter/D < 0.75:
-            print('-1')
+        if counter != x:
+            print(-1)
